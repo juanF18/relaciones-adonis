2795d17643f10bd256f4c9c282493d9e39fafec0
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2022-11-10 13:14:24 -0500

    Initial commit

---
+++ database/migrations/1668050856584_usuarios.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'usuarios'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      //referencia al id del rol es una foreign key
+      table.integer('id_rol').unsigned().references('roles.id')
+      table.string('nombre', 60)
+      table.string('correo', 254)
+      table.string('contrasena', 256)
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
---
+++ database/migrations/1668051050086_permisos_rols.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'permisos_rol'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('id_rol').unsigned().references('roles.id').onDelete('CASCADE')
+      table.integer('id_permiso').unsigned().references('permisos.id').onDelete('CASCADE')
+      table.unique(['id_rol', 'id_permiso'])
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
